84487289ebcdee968324af8e450f285eed57f40a
Capset14 <infotechcapset1@gmail.com>
2025-01-23 11:48:25 +0530

    main

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
+        iconTheme: const IconThemeData(
+          color: Colors.amber
+        ),
         brightness: Brightness.dark,
         // This is the theme of your application.
         //
